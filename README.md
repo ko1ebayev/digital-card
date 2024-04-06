68c315813984952aecd718b9e874c2cdb7d79846
ko1ebayev <ko1ebayev.worx@gmail.com>
2024-04-07 00:31:38 +0300

    setup template

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# digital-card
+# Landing Page Starter
+
+## Available commends
+
+- `build` - build application,
+- `serve` - run dev server,
+- `lint` - lint js files.
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './styles.scss';
+
+// Import fonts when you use customs
+// import './fonts/BebasNeueRegular-gxj83.ttf';
+
+document.addEventListener('DOMContentLoaded', initPage);
+
+function initPage() {}
